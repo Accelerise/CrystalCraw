d0779c37f1a47d2fd9a6c7e6e5bf56c0bcdcf6d4
wcgfech <271964484@qq.com>
2017-09-06 08:33:12 +0800

    no message

--- wcgfetch/craw/Downloader.py
+++ wcgfetch/craw/Downloader.py
@@ -21,8 +21,8 @@ class Downloader:
 		self.options.add_argument('window-size=1200x600')
 		self.options.add_argument('load-images=no')  ##关闭图片加载
 		self.driver = webdriver.Chrome(chrome_options=self.options)
-		self.driver.set_page_load_timeout(self.page_load_time)
-		self.driver.set_script_timeout(self.script_time)
+		self.driver.set_page_load_timeout(Downloader.page_load_time)
+		self.driver.set_script_timeout(Downloader.script_time)
 		self.cnt = 0
 
 	# 单例模式
@@ -62,8 +62,8 @@ class Downloader:
 		page = self.driver.find_elements_by_xpath("/html")[0].get_attribute("innerHTML")
 		self.driver.quit()
 		self.driver = webdriver.Chrome(chrome_options=self.options)
-		self.driver.set_page_load_timeout(self.page_load_time)
-		self.driver.set_script_timeout(self.script_time)
+		self.driver.set_page_load_timeout(Downloader.page_load_time)
+		self.driver.set_script_timeout(Downloader.script_time)
 		return page
 
 	# void 关闭Chrome下载器
